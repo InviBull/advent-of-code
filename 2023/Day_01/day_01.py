8549b7da0e34965e210d44d2ab30fea11f0834db
InviBull <virtualclass.raghav@gmail.com>
2023-12-01 21:15:52 +0530

    2023: Day_01

--- 2023/Day_01/day_01.py
+++ 2023/Day_01/day_01.py
@@ -20,7 +20,8 @@ print(p1)
 
 # p2
 p2 = 0
-digits = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+digits = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8",
+          "nine": "9"}
 
 for word in data:
     x = ""
